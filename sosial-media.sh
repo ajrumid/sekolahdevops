50a34e7fc008ef95f03de767e2e9b3b724dbd832
ajrumid <129446427+ajrumid@users.noreply.github.com>
2023-03-31 12:53:01 +0700

    sosial-media.sh

---
+++ sosial-media.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Menyiapkan Installasi Web server"
+apt-get update
+echo "Melakukan Installasi Webserver"
+apt-get install -y apache2 zip unzip
+echo "Download file apps landing page"
+wget https://github.com/sdcilsy/sosial-media/archive/refs/heads/master.zip
+echo "Memindahkan data"
+unzip master.zip
+rm -rf /var/www/html/* | mv sosial-media-master/* /var/www/html
+echo "restart service apache2"
+systemctl restart apache2
